1fabc13a5580580272d9de0140b0aa61c6661df8
JergeRG <rodriguezg.jorgea@gmail.com>
2023-07-16 00:02:31 -0600

    S3 configuration added

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,18 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run(){
-    core.notice('Hello form my custom JavaScript Action.');
+    // Get input values
+    const bucketName = core.getInput('bucket-name', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', {required: true }); 
+
+
+    // Upload files to S3
+    const s3Uri = `s3://${bucketName}`;
+    core.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    const websiteUrl = `http://${bucketName}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl); // run: echo 'website-url=${websiteUrl}' >> $GITHUB_OUTPTU
 }
 
 run()
